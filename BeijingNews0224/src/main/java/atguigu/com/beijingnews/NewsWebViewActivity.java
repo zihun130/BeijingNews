b0bf3ad9751e80b4ac307c5bfc5b292c7969c0a4
zihun130 <zihun130@126.com>
2017-06-06 11:43:45 +0800

    设置WebView的字体大小

--- BeijingNews0224/src/main/java/atguigu/com/beijingnews/NewsWebViewActivity.java
+++ BeijingNews0224/src/main/java/atguigu/com/beijingnews/NewsWebViewActivity.java
@@ -1,5 +1,7 @@
 package atguigu.com.beijingnews;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -35,6 +37,9 @@ public class NewsWebViewActivity extends AppCompatActivity {
     @InjectView(R.id.progressbar)
     ProgressBar progressbar;
     private Uri Url;
+    private WebSettings settings;
+    private int tempIndex=2;
+    private int realIndex=tempIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +51,7 @@ public class NewsWebViewActivity extends AppCompatActivity {
 
         Url=getIntent().getData();
 
-        WebSettings settings = webview.getSettings();
+        settings = webview.getSettings();
         //设置支持JavaScript
         settings.setJavaScriptEnabled(true);
         //设置双击页面变大变小
@@ -82,10 +87,51 @@ public class NewsWebViewActivity extends AppCompatActivity {
                 finish();
                 break;
             case R.id.ib_textsize:
-                Toast.makeText(NewsWebViewActivity.this, "设置文字大小", Toast.LENGTH_SHORT).show();
+                ShowChangerTextSize();
                 break;
             case R.id.ib_share:
                 Toast.makeText(NewsWebViewActivity.this, "分享", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    private void ShowChangerTextSize() {
+        String[] items={"超大字体","大字体","正常字体","小字体","超小字体"};
+        new AlertDialog.Builder(this)
+                    .setTitle("设置文字大小")
+                    .setSingleChoiceItems(items, 2, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                               tempIndex=which;
+                        }
+                    })
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                              realIndex=tempIndex;
+                            ChangerSize(realIndex);
+                        }
+                    })
+                    .setNegativeButton("取消", null)
+                    .show();
+    }
+
+    private void ChangerSize(int Index) {
+        switch (Index) {
+            case  0:
+                settings.setTextZoom(200);
+                break;
+            case  1:
+                settings.setTextZoom(150);
+                break;
+            case  2:
+                settings.setTextZoom(100);
+                break;
+            case  3:
+                settings.setTextZoom(75);
+                break;
+            case  4:
+                settings.setTextZoom(50);
                 break;
         }
     }
